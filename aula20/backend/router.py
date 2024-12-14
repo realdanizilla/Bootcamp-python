72e9eaf6dd5772f7e20adb9a04859973add3ad87
Daniel Ribeiro <dari@jiffygroup.net>
2024-12-14 12:11:11 -0300

    fixed router and docker-compose

--- aula20/backend/router.py
+++ aula20/backend/router.py
@@ -37,12 +37,12 @@ def read_one_product(product_id: int, db: Session = Depends(get_db)):
 
 ### criar rota de adicionar 1 item
 @router.post("/products/", response_model=ProductResponse)
-def create_product(product: ProductCreate, db: Session = Depends(get_db)):
-    return create_product(product=product, db=db)
+def create_product_route(product: ProductCreate, db: Session = Depends(get_db)):
+    return create_product(db=db, product=product)
 
 ### criar rota de deletar 1 item
 @router.delete("/products/{product_id}", response_model=ProductResponse)
-def delete_product(product_id: int, db: Session = Depends(get_db)):
+def delete_product_route(product_id: int, db: Session = Depends(get_db)):
     db_product = delete_product(db, product_id=product_id)
     if db_product is None:
          raise HTTPException(status_code=404, detail="voce está deletando um produto que nao existe")
@@ -50,8 +50,8 @@ def delete_product(product_id: int, db: Session = Depends(get_db)):
 
 ### criar rota de fazer update nos itens
 @router.put("/products/{product_id}", response_model=ProductResponse)
-def update_product(product_id: int, product: ProductUpdate , db: Session = Depends(get_db)):
-    db_product = update_product(db=db, product_id=product_id, product=product)
+def update_product_route(product_id: int, product: ProductUpdate , db: Session = Depends(get_db)):
+    db_product = update_product(db, product_id=product_id, product=product)
     if db_product is None:
          raise HTTPException(status_code=404, detail="voce está fazendo update em um produto que nao existe")
     return db_product
